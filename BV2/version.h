11e312d8c165377c84d34588040d1bd8f71b4da5
eshabe <eshabe@42c07b21-b953-0410-b846-bb8d69ff4585>
2008-08-23 20:02:45 +0000

    Some minor change. I m learning bout svn.

--- BV2/version.h
+++ BV2/version.h
@@ -18,10 +18,14 @@
 	static const long REVISION = 1;
 	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT = 3;
+	static const long BUILDS_COUNT = 7;
 	#define RC_FILEVERSION 2,1,2,1
 	#define RC_FILEVERSION_STRING "2, 1, 2, 1\0"
 	static const char FULLVERSION_STRING[] = "2.1.2.1";
+	
+	//SVN Version
+	static const char SVN_REVISION[] = "0";
+	static const char SVN_DATE[] = "unknown date";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY = 1;
